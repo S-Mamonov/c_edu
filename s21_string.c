a0cdf8f7d70ec2ce44d7b5b9c74e0c328313f7a0
S-Mamonov <141213481+S-Mamonov@users.noreply.github.com>
2024-06-08 16:32:05 +0700

    add upper and lower

--- s21_string.c
+++ s21_string.c
@@ -1,29 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
-#define s21_NULL (void*)0
-
-typedef unsigned long s21_size_t;
-
-void* s21_memchr(const void* str, int c, s21_size_t n);
-int s21_memcmp(const void *str1, const void *str2, s21_size_t n);
-void* s21_memcpy(void* dest, const void* src, s21_size_t n);
-void* s21_memset(void* str, int c, s21_size_t n);
-char* s21_strncat(char* dest, const char* src, s21_size_t n);
-char* s21_strchr(const char* str, int c);
-int s21_strncmp(const char* str1, const char* str2, s21_size_t n);
-char* s21_strncpy(char* dest, const char* src, s21_size_t n);
-s21_size_t s21_strcspn(const char* str1, const char* str2);
-
-s21_size_t s21_strlen(const char* str);
-char* s21_strpbrk(const char* str1, const char* str2);
-char* s21_strrchr(const char* str, int c);
-char* s21_strstr(const char* haystack, const char* needle);
-char* s21_strtok(char* str, const char* delim);
-
-void* s21_insert(const char* src, const char* str, s21_size_t start_index);
-void* s21_trim(const char* src, const char* trim_chars);
+#include "s21_string.h"
 
 int main()
 {
@@ -35,6 +12,7 @@ int main()
     char str2[] = "ab";
 
     printf("--%s--\n", (char*)s21_trim(str, str2));
+    printf("--%s--\n", (char*)s21_to_upper(str2));
 
     return 0;
 }
@@ -216,6 +194,38 @@ char* s21_strtok(char* str, const char* delim){
     }
 
     return not_null ? str : s21_NULL;
+}
+
+void* s21_to_upper(const char* str){
+    char* res = s21_NULL;
+
+    if (str){
+        res = calloc(s21_strlen(str) + 1, sizeof(char));
+        for (s21_size_t i = 0; str[i]; i++){
+            if (str[i] >= 'a' && str[i] <= 'z'){
+                res[i] = str[i] - 32;
+            }
+            else
+                res[i] = str[i];
+        }
+    }
+    return res;
+}
+
+void* s21_to_lower(const char* str){
+    char* res = s21_NULL;
+
+    if (str){
+        res = calloc(s21_strlen(str) + 1, sizeof(char));
+        for (s21_size_t i = 0; str[i]; i++){
+            if (str[i] >= 'A' && str[i] <= 'Z'){
+                res[i] = str[i] + 32;
+            }
+            else
+                res[i] = str[i];
+        }
+    }
+    return res;
 }
 
 void* s21_insert(const char* src, const char* str, s21_size_t start_index){
